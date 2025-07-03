cdc3718a93f6eadebe643c3579021e02716b62b8
GoldenAnpu <seryios.sych@gmail.com>
2025-07-03 14:51:36 +0200

    Update docker-compose.yml and globals.py for improved API configuration and logging
    - Specify Dockerfile path in docker-compose.yml
    - Add SUPERVISELY_API_SERVER_ADDRESS environment variable
    - Enhance logging for API token retrieval in globals.py

--- src/globals.py
+++ src/globals.py
@@ -9,7 +9,25 @@ if sly.is_development():
     load_dotenv(os.path.expanduser("~/supervisely.env"))
     load_dotenv("local.env")
 
-api = sly.Api(ignore_task_id=True)
+internal_address = os.getenv("SUPERVISELY_API_SERVER_ADDRESS", None)
+sly.logger.debug("Internal Supervisely API server address: %s", internal_address)
+api_token = os.getenv("API_TOKEN", None)
+sly.logger.debug("API token from environment: %s", api_token)
+if (internal_address is not None and internal_address != "") and (api_token is None or api_token == ""):
+    temp_api = sly.Api(ignore_task_id=True)
+    response = temp_api.post(
+        "instance.admin-info", data={"accessToken": "SUPERVISELY_API_ACCESS_TOKEN"}
+    )
+    token = response.json()["id"]
+    sly.logger.debug("Using Supervisely API token: %s", token)
+elif api_token is not None and api_token != "":
+    token = api_token
+    sly.logger.debug("Using Supervisely API token from environment: %s", token)
+else:
+    token = None
+    sly.logger.debug("No internal Supervisely API server address found, using public API")
+
+api = sly.Api(ignore_task_id=True, token=token)
 sly.logger.debug("Connected to Supervisely API: %s", api.server_address)
 api.file.load_dotenv_from_teamfiles(override=True)
 clip_slug = "supervisely-ecosystem/deploy-clip-as-service"
@@ -20,7 +38,7 @@ qdrant_host = os.getenv("modal.state.qdrantHost") or os.getenv("QDRANT_HOST")
 clip_host = os.getenv("modal.state.clipHost", None) or os.getenv("CLIP_HOST", None)
 
 sly.logger.debug("CLIP host from environment: %s", clip_host)
-if clip_host is None:
+if clip_host is None or "":
     clip_host = get_app_host(api, clip_slug)
 
 try:
